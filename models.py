a51a700e28fcb8bf5299e1f2e448cac25ffd7fdd
Delor Rotstrump <delor4@gmail.com>
2019-07-13 10:44:19 +0200

    refactor: User -> Profile

--- models.py
+++ models.py
@@ -5,16 +5,16 @@ db = Database()
 
 
 class Note(db.Entity):
-    _table_ = 'note'
+    _table_ = 'notes'
     id = PrimaryKey(int, column='note_id', auto=True)
     date = Required(date, column='note_date', index='date_index')
     textnotes = Set('TextNote')
     images = Set('Image')
-    user = Required('User')
+    profile = Required('Profile')
 
 
 class TextNote(db.Entity):
-    _table_ = 'text_note'
+    _table_ = 'text_notes'
     id = PrimaryKey(int, column='text_note_id', auto=True)
     note = Required(Note, column='note_id')
     value = Required(LongStr, column='text_note_value')
@@ -22,16 +22,17 @@ class TextNote(db.Entity):
 
 
 class Image(db.Entity):
-    _table_ = 'image'
+    _table_ = 'images'
     id = PrimaryKey(int, column='image_id', auto=True)
     note = Required(Note)
     path = Required(str, column='image_path')
     geo_coord = Optional(Json)
 
 
-class User(db.Entity):
-    id = PrimaryKey(int, column='user_id', auto=True)
-    name = Required(str, unique=True, column='user_name')
+class Profile(db.Entity):
+    _table_ = 'profiles'
+    id = PrimaryKey(int, column='profile_id', auto=True)
+    name = Required(str, unique=True, column='profile_name')
     notes = Set(Note)
 
 
@@ -42,9 +43,9 @@ def add_textnote(day_date: date, note: str) -> None:
     :param day_date: selected day
     :param note: text to add
     """
-    n = Note.get(date=day_date, user=User[curr_user_id])
+    n = Note.get(date=day_date, profile=Profile[curr_profile_id])
     if n is None:
-        n = Note(date=day_date, user=User[curr_user_id])
+        n = Note(date=day_date, profile=Profile[curr_profile_id])
     TextNote(value=note, note=n)
 
 
@@ -55,7 +56,7 @@ def get_notes(day_date: date) -> TextNote:
     :param day_date: selected day
     :return: TextNote (readonly values)
     """
-    n = Note.get(date=day_date, user=User[curr_user_id])
+    n = Note.get(date=day_date, profile=Profile[curr_profile_id])
     for i in TextNote.select(lambda j: j.note == n):
         _, _, _ = i.id, i.value, i.geo_coord  # for writing to cache
         yield i
@@ -68,7 +69,7 @@ def has_textnotes(day_date: date) -> bool:
     :param day_date: selected day
     :return: bool
     """
-    n = Note.get(date=day_date, user=User[curr_user_id])
+    n = Note.get(date=day_date, profile=Profile[curr_profile_id])
     if n is None or len(n.textnotes) == 0:
         return False
     return True
@@ -105,9 +106,9 @@ def add_image(day_date: date, path: str) -> None:
     :param day_date:
     :param path: Path to image file (relative to app images folder).
     """
-    n = Note.get(date=day_date, user=User[curr_user_id])
+    n = Note.get(date=day_date, profile=Profile[curr_profile_id])
     if n is None:
-        n = Note(date=day_date, user=User[curr_user_id])
+        n = Note(date=day_date, profile=Profile[curr_profile_id])
     Image(path=path, note=n)
 
 
@@ -118,7 +119,7 @@ def get_images(day_date: date) -> Image:
     :param day_date: selected date
     :return: Image (readonly values)
      """
-    n = Note.get(date=day_date, user=User[curr_user_id])
+    n = Note.get(date=day_date, profile=Profile[curr_profile_id])
     for i in Image.select(lambda j: j.note == n):
         _, _, _ = i.id, i.path, i.geo_coord  # for writing to cache
         yield i
@@ -131,7 +132,7 @@ def has_images(day_date: date) -> bool:
     :param day_date: selected day
     :return: bool
     """
-    n = Note.get(date=day_date, user=User[curr_user_id])
+    n = Note.get(date=day_date, profile=Profile[curr_profile_id])
     if n is None or len(n.images) == 0:
         return False
     return True
@@ -163,23 +164,23 @@ def delete_image(im_id: int) -> None:
 
 @db_session
 def init_db() -> int:
-    user_count = select(u.id for u in User).count()
+    user_count = select(u.id for u in Profile).count()
     if user_count == 0:
-        u = User(name='Default profile')
+        u = Profile(name='Default profile')
         commit()
         return u.id
 
 
 @db_session
 def make_profile(name: str) -> int:
-    u = User(name=name)
+    u = Profile(name=name)
     commit()
     return u.id
 
 
 @db_session
 def update_profile(profile_id: int, name: str) -> int:
-    u = User[profile_id]
+    u = Profile[profile_id]
     if name is not None:
         u.name = name
     commit()
@@ -188,20 +189,20 @@ def update_profile(profile_id: int, name: str) -> int:
 
 @db_session
 def set_curr_profile(profile_id: int) -> int:
-    u = User[profile_id]
-    global curr_user_id
-    tmp = curr_user_id
-    curr_user_id = u.id
+    u = Profile[profile_id]
+    global curr_profile_id
+    tmp = curr_profile_id
+    curr_profile_id = u.id
     return tmp
 
 
 def get_curr_profile() -> int:
-    return curr_user_id
+    return curr_profile_id
 
 
 @db_session
 def get_curr_profile_name() -> str:
-    u = User[get_curr_profile()]
+    u = Profile[get_curr_profile()]
     return u.name
 
 
@@ -213,4 +214,4 @@ db.bind(provider='sqlite', filename=':memory:')
 
 db.generate_mapping(create_tables=True)
 
-curr_user_id = init_db()
+curr_profile_id = init_db()
