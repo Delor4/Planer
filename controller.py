c229831b95740199f4972c512b5380d06233a7bb
Delor Rotstrump <delor4@gmail.com>
2019-07-15 00:11:56 +0200

    changes

--- controller.py
+++ controller.py
@@ -1,49 +1,76 @@
-import calendar
 import datetime
 import models
-
-db = models.PlanerDB()
+import calendar
 
 
 class Calendar:
-    def __init__(self, parent):
-
-        self.parent = parent
-        self.cal = calendar.Calendar
-        self.day_selected = datetime.date.today().day
-        self.year = datetime.date.today().year
-        self.month = datetime.date.today().month
-        self.month_selected = self.month
-        self.year_selected = self.year
-        self.refresh(self.year, self.month)
+    def __init__(self, _=None):
+        d = datetime.date.today()
+        self.date = datetime.date(d.year, d.month, 1)
+        self.db = models.PlanerDB()
 
     def prev_month(self):  # ustawienie poprzedniego miesiąca
-        if self.month > 1:
-            self.month -= 1
-        else:
-            self.month = 12
-            self.month -= 1
-
-            self.refresh(self.year, self.month)
+        d = self.date - datetime.timedelta(28)
+        self.date = datetime.date(d.year, d.month, 1)
 
     def next_month(self):  # ustawienie następnego miesiąca
-        if self.month < 12:
-            self.month += 1
-        else:
-            self.month = 1
-            self.month += 1
-            self.refresh(self.year, self.month)
+        d = self.date + datetime.timedelta(31)
+        self.date = datetime.date(d.year, d.month, 1)
+
+    def _make_date(self, day):
+        return datetime.date(self.date.year, self.date.month, day)
 
     def get_day_data(self, day):
+        notes = []
+        for i in self.db.get_notes(self._make_date(day)):
+            notes.append(
+                {'id': i.id,
+                 'values': i.value,
+                 'geo_coord': i.geo_coord
+                 }
+            )
+        images = []
+        for i in self.db.get_images(self._make_date(day)):
+            images.append(
+                {'id': i.id,
+                 'path': i.path,
+                 'geo_coord': i.geo_coord
+                 }
+            )
+        return notes, images
+
+    def get_day_data2(self, day):
         self.day_selected = day
         self.month_selected = self.month
         self.year_selected = self.year
-        for i in db.get_notes(self.year_selected, self.month_selected, self.day_selected):
+        for i in self.db.get_notes(self.date.year, self.month_selected, self.day_selected):
             print("test" + i.id + i.values)
 
-    def get_month_data(self, month):
-        self.month_selected = month
-        self.year_selected = self.year
+    def get_month_data(self):
+        ret = []
+        cal = calendar.Calendar()
+        # row = 0
+        for day in cal.itermonthdays2(self.date.year, self.date.month):
+            if day[0] > 0:
+                b = {'day_of_month': day[0],
+                     # 'week_of_month': row,
+                     'day_of_week': day[1],
+                     'notes_count': self.db.count_textnotes(datetime.date(self.date.year, self.date.month, day[0])),
+                     'images_count': self.db.count_images(datetime.date(self.date.year, self.date.month, day[0]))
+                     }
+                ret.append(b)
+            # if day[1] == 6:
+            #    row += 1
+        return ret
+
+    def get_month_data_g(self):
+        for day in calendar.Calendar().itermonthdays2(self.date.year, self.date.month):
+            if day[0] > 0:
+                yield {'day_of_month': day[0],
+                       'day_of_week': day[1],
+                       'notes_count': self.db.count_textnotes(datetime.date(self.date.year, self.date.month, day[0])),
+                       'images_count': self.db.count_images(datetime.date(self.date.year, self.date.month, day[0]))
+                       }
 
     def refresh(self, y, m):
         self.year_selected = y
