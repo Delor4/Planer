aac872e7d85f64643397ba62515a7acedd201761
Delor Rotstrump <delor4@gmail.com>
2019-07-15 10:09:58 +0200

    mocks

--- controller.py
+++ controller.py
@@ -25,7 +25,7 @@ class Calendar:
         for i in self.db.get_notes(self._make_date(day)):
             notes.append(
                 {'id': i.id,
-                 'values': i.value,
+                 'value': i.value,
                  'geo_coord': i.geo_coord
                  }
             )
@@ -75,3 +75,51 @@ class Calendar:
                        }
             if day[1] == 6:
                 row += 1
+
+    def get_month_data_mock(self):
+        return [
+            {'day_of_month': 1, 'week_of_month': 0, 'day_of_week': 4, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 2, 'week_of_month': 0, 'day_of_week': 5, 'notes_count': 1, 'images_count': 1},
+            {'day_of_month': 3, 'week_of_month': 0, 'day_of_week': 6, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 4, 'week_of_month': 1, 'day_of_week': 0, 'notes_count': 0, 'images_count': 5},
+            {'day_of_month': 5, 'week_of_month': 1, 'day_of_week': 1, 'notes_count': 1, 'images_count': 0},
+            {'day_of_month': 6, 'week_of_month': 1, 'day_of_week': 2, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 7, 'week_of_month': 1, 'day_of_week': 3, 'notes_count': 0, 'images_count': 1},
+            {'day_of_month': 8, 'week_of_month': 1, 'day_of_week': 4, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 9, 'week_of_month': 1, 'day_of_week': 5, 'notes_count': 6, 'images_count': 2},
+            {'day_of_month': 10, 'week_of_month': 1, 'day_of_week': 6, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 11, 'week_of_month': 2, 'day_of_week': 0, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 12, 'week_of_month': 2, 'day_of_week': 1, 'notes_count': 9, 'images_count': 3},
+            {'day_of_month': 13, 'week_of_month': 2, 'day_of_week': 2, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 14, 'week_of_month': 2, 'day_of_week': 3, 'notes_count': 0, 'images_count': 15},
+            {'day_of_month': 15, 'week_of_month': 2, 'day_of_week': 4, 'notes_count': 2, 'images_count': 0},
+            {'day_of_month': 16, 'week_of_month': 2, 'day_of_week': 5, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 17, 'week_of_month': 2, 'day_of_week': 6, 'notes_count': 32, 'images_count': 0},
+            {'day_of_month': 18, 'week_of_month': 3, 'day_of_week': 0, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 19, 'week_of_month': 3, 'day_of_week': 1, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 20, 'week_of_month': 3, 'day_of_week': 2, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 21, 'week_of_month': 3, 'day_of_week': 3, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 22, 'week_of_month': 3, 'day_of_week': 4, 'notes_count': 87, 'images_count': 54},
+            {'day_of_month': 23, 'week_of_month': 3, 'day_of_week': 5, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 24, 'week_of_month': 3, 'day_of_week': 6, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 25, 'week_of_month': 4, 'day_of_week': 0, 'notes_count': 4, 'images_count': 0},
+            {'day_of_month': 26, 'week_of_month': 4, 'day_of_week': 1, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 27, 'week_of_month': 4, 'day_of_week': 2, 'notes_count': 0, 'images_count': 0},
+            {'day_of_month': 28, 'week_of_month': 4, 'day_of_week': 3, 'notes_count': 0, 'images_count': 0}
+        ]
+
+    def get_day_data_mock(self, _):
+        return [
+                   {'id': 1, 'value': 'Lorem ipsum dolor sit amet ...', 'geo_coord': {}},
+                   {'id': 2,
+                    'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi.',
+                    'geo_coord': {}},
+                   {'id': 3,
+                    'value': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.',
+                    'geo_coord': {}}
+               ],
+        [
+            {'id': 1, 'path': 'image1.jpg', 'geo_coord': {}},
+            {'id': 2, 'path': 'image2.jpg', 'geo_coord': {}},
+            {'id': 3, 'path': 'image3.jpg', 'geo_coord': {}}
+        ]
--- examples_controller.py
+++ examples_controller.py
@@ -35,8 +35,12 @@ def test_list():
     return a
 
 
-c.db.add_textnote(datetime.date(c.date.year, c.date.month, 5), "gfgfg")
-
+c.db.add_textnote(datetime.date(c.date.year, c.date.month, 5), "Lorem ipsum dolor sit amet ...")
+c.db.add_textnote(datetime.date(c.date.year, c.date.month, 5), "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi.")
+c.db.add_textnote(datetime.date(c.date.year, c.date.month, 5), "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin nibh augue, suscipit a, scelerisque sed, lacinia in, mi. Cras vel lorem. Etiam pellentesque aliquet tellus. Phasellus pharetra nulla ac diam. Quisque semper justo at risus. Donec venenatis, turpis vel hendrerit interdum, dui ligula ultricies purus, sed posuere libero dui id orci. Nam congue, pede vitae dapibus aliquet, elit magna vulputate arcu, vel tempus metus leo non est. Etiam sit amet lectus quis est congue mollis. Phasellus congue lacus eget neque. Phasellus ornare, ante vitae consectetuer consequat, purus sapien ultricies dolor, et mollis pede metus eget nisi. Praesent sodales velit quis augue. Cras suscipit, urna at aliquam rhoncus, urna quam viverra nisi, in interdum massa nibh nec erat.")
+c.db.add_image(datetime.date(c.date.year, c.date.month, 5), "image1.jpg")
+c.db.add_image(datetime.date(c.date.year, c.date.month, 5), "image2.jpg")
+c.db.add_image(datetime.date(c.date.year, c.date.month, 5), "image3.jpg")
 for t in c.get_month_data_():
     print(t)
 
