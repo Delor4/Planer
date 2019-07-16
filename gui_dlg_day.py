4b922a6bf0218684db8f75428026b0fbc7480b94
Delor Rotstrump <delor4@gmail.com>
2019-07-16 22:42:28 +0200

    added adding new images to DayDialog

--- gui_dlg_day.py
+++ gui_dlg_day.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
+import os
 
 
 class DayDialog:
@@ -17,6 +19,7 @@ class DayDialog:
             self.parent = parent
 
             self.notes_data = []
+            self.images_data = []
 
             self.notes_frame = None
             self.initUI()
@@ -36,18 +39,29 @@ class DayDialog:
             id = self.state.add_textnote(self.day, txt)
             self.make_frame_note(self.notes_frame, {'id': id, 'value': txt})
 
+        def create_new_image(self):
+            filename = filedialog.askopenfilename(initialdir=".", title="Select file",
+                                                  filetypes=(("jpeg files", "*.jpg"), ("all files", "*.*")))
+            if os.path.isfile(filename):
+                id = self.state.add_image(filename)
+                im = self.state.get_image(id)
+                self.make_frame_image(self.images_frame, {'id': im.id,
+                                                          'path': im.path,
+                                                          'geo_coord': im.geo_coord
+                                                          })
+
         def initUI(self):
             notes, images = self.state.get_day_data(self.day)
             frame = ttk.Frame(self.window)
             frame.pack(fill=BOTH)
 
             self.notes_frame = self.make_notes_frame(frame, notes)
-            images_frame = self.make_images_frame(frame, images)
+            self.images_frame = self.make_images_frame(frame, images)
             buttons_frame = self.make_buttons_frame(frame)
 
             buttons_frame.pack(side=BOTTOM, anchor=E)
             self.notes_frame.pack(side=LEFT, fill=BOTH, padx=2, pady=2)
-            images_frame.pack(side=LEFT, fill=BOTH, padx=2, pady=2)
+            self.images_frame.pack(side=LEFT, fill=BOTH, padx=2, pady=2)
 
         def make_frame_note(self, frame, note_data):
             f = Frame(frame)
@@ -60,6 +74,9 @@ class DayDialog:
             f.pack()
             self.notes_data.append({'id': note_data['id'], 'old_value': note_data['value'], 'text_widget': tn})
 
+        def make_frame_image(self, frame, image_data):
+            ttk.Label(frame, text=image_data['path']).pack()
+
         def make_notes_frame(self, frame, notes):
             notes_frame = ttk.LabelFrame(frame, text="Notatki")
             ttk.Button(notes_frame, text="+", command=self.create_new_note).pack()
@@ -71,12 +88,12 @@ class DayDialog:
 
         def make_images_frame(self, frame, images):
             images_frame = ttk.LabelFrame(frame, text="Obrazy")
-            ttk.Button(images_frame, text="+").pack()
+            ttk.Button(images_frame, text="+", command=self.create_new_image).pack()
 
             if len(images) == 0:
                 ttk.Label(images_frame, text="Brak obrazów.").pack()
             for i in images:
-                ttk.Label(images_frame, text=i['path']).pack()
+                self.make_frame_image(images_frame, i)
             return images_frame
 
         def make_buttons_frame(self, frame):
