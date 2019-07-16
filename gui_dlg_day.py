e92e78d7dc2fa21fcb875f892c1d6a1c485a003a
Delor Rotstrump <delor4@gmail.com>
2019-07-16 18:41:12 +0200

    refactor

--- gui_dlg_day.py
+++ gui_dlg_day.py
@@ -31,15 +31,25 @@ class DayDialog:
             notes, images = self.state.get_day_data(self.day)
             frame = ttk.Frame(self.window)
             frame.pack(fill=BOTH)
-            # frame = self
 
+            notes_frame = self.make_notes_frame(frame, notes)
+            images_frame = self.make_images_frame(frame, images)
+            buttons_frame = self.make_buttons_frame(frame)
+
+            buttons_frame.pack(side=BOTTOM, anchor=E)
+            notes_frame.pack(side=LEFT, fill=BOTH, padx=2, pady=2)
+            images_frame.pack(side=LEFT, fill=BOTH, padx=2, pady=2)
+
+        def make_notes_frame(self, frame, notes):
             notes_frame = ttk.LabelFrame(frame, text="Notatki")
             ttk.Button(notes_frame, text="+").pack()
             if len(notes) == 0:
                 ttk.Label(notes_frame, text="Brak notatek.").pack()
             for n in notes:
                 ttk.Label(notes_frame, text=n['value']).pack()
+            return notes_frame
 
+        def make_images_frame(self, frame, images):
             images_frame = ttk.LabelFrame(frame, text="Obrazy")
             ttk.Button(images_frame, text="+").pack()
 
@@ -47,12 +57,10 @@ class DayDialog:
                 ttk.Label(images_frame, text="Brak obrazów.").pack()
             for i in images:
                 ttk.Label(images_frame, text=i['path']).pack()
+            return images_frame
 
+        def make_buttons_frame(self, frame):
             buttons_frame = ttk.Frame(frame)
             ttk.Button(buttons_frame, text='Anuluj', command=self.parent.cancel).pack(side=RIGHT)
             ttk.Button(buttons_frame, text='Ok', command=self.parent.ok).pack(side=RIGHT)
-
-            buttons_frame.pack(side=BOTTOM, anchor=E)
-
-            notes_frame.pack(side=LEFT, fill=BOTH, padx=2, pady=2)
-            images_frame.pack(side=LEFT, fill=BOTH, padx=2, pady=2)
+            return buttons_frame
