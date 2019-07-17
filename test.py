cd1e1a8284761d9c8b463ef3350bf2e18676e34c
Delor Rotstrump <delor4@gmail.com>
2019-07-17 10:03:13 +0200

    example in test.py

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import calendar
+
 cal = calendar.Calendar()
 row = 0
 for day in cal.itermonthdays2(2016, 1):
@@ -6,3 +7,13 @@ for day in cal.itermonthdays2(2016, 1):
         print("col:{0}, row:{1}".format(day[1], row))
     if day[1] == 6:
         row += 1
+
+print(
+    '\n'.join(
+        ["SKS Team:",
+         "Brodziak Sebastian",
+         "Jaśkowski Krzysztof",
+         "Kucharczyk Sebastian"
+         ]
+    )
+)
