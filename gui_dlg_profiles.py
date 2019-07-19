7e330c09f164261787c7a5dd156fc2308480c8ab
Delor Rotstrump <delor4@gmail.com>
2019-07-19 14:45:55 +0200

    edit profile name

--- gui_dlg_profiles.py
+++ gui_dlg_profiles.py
@@ -5,6 +5,7 @@ from tkinter import Toplevel
 
 class ProfilesDialog:
     def __init__(self, app):
+        self.app = app
         self.state = app.state
         self.T = app.T
         self.top = Toplevel(app.main_window)
@@ -13,6 +14,8 @@ class ProfilesDialog:
         app.set_icon(self.top)
         self.top.title("{0} - {1}".format(app.app_name, self.T("profiles_config_title")))  # Profiles config
         self.main_frame = None
+        self.input_ok = False
+        self.input_top = None
         self.init_ui()
 
     def refresh(self):
@@ -26,6 +29,12 @@ class ProfilesDialog:
 
     def on_select_profile(self, profile_id):
         self.state.set_current_profile(profile_id)
+        self.refresh()
+
+    def on_edit_profile(self, profile_id):
+        text = self.get_user_text("enter_profile_name")  # Wprowadź nazwę profilu:
+        if text is not None and len(text) > 0:
+            self.state.update_profile(profile_id, text)
         self.refresh()
 
     def on_exit(self):
@@ -45,7 +54,8 @@ class ProfilesDialog:
             if self.state.get_curr_profile() == p['id']:
                 name_label.config(bg="white")
             Button(profile_frame, text=self.T("delete").capitalize()).pack(side=RIGHT, anchor=E)
-            Button(profile_frame, text=self.T("edit").capitalize()).pack(side=RIGHT)
+            Button(profile_frame, text=self.T("edit").capitalize(),
+                   command=lambda pid=p['id']: self.on_edit_profile(pid)).pack(side=RIGHT)
             Button(profile_frame, text=self.T("select").capitalize(),
                    command=lambda pid=p['id']: self.on_select_profile(pid)).pack(side=RIGHT)
             profile_frame.pack(fill=X, anchor=E)
@@ -57,3 +67,26 @@ class ProfilesDialog:
         add_frame.pack()
         bottom_frame.pack(fill=X, side=BOTTOM)
         self.main_frame.pack()
+
+    def get_user_text(self, prompt, title=None):
+        input_top = Toplevel(self.top)
+        self.input_top = input_top
+
+        input_top.transient(self.top)
+        input_top.grab_set()
+        self.app.set_icon(input_top)
+        if title is None:
+            title = self.app.app_name
+        input_top.title(title)
+        value = StringVar()
+        self.input_ok = False
+        Label(input_top, text=prompt).pack(anchor=W)
+        Entry(input_top, textvariable=value).pack(fill=X)
+        Button(input_top, text=self.T("ok"), command=self.on_input_ok).pack(anchor=E)
+        self.top.wait_window(input_top)
+        if self.input_ok:
+            return value.get()
+
+    def on_input_ok(self):
+        self.input_ok = True
+        self.input_top.destroy()
